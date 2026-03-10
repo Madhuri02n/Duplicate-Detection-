e4861a9e0372e5b0b1c8a5c6d2b1cfb925bbbc76
Nallabothula Madhuri <139317419+Madhuri02n@users.noreply.github.com>
2026-03-10 16:25:07 +0530

    Remove unnecessary whitespace and format code

--- DuplicateEmails.java
+++ DuplicateEmails.java
@@ -2,12 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class DuplicateEmails {
-
     public static void main(String[] args) throws Exception {
-
         HashSet<String> seen = new HashSet<>();
         HashSet<String> duplicates = new HashSet<>();
-
         BufferedReader br = new BufferedReader(new FileReader("emails.txt"));
         String email;
 
@@ -19,13 +16,11 @@ public class DuplicateEmails {
                 seen.add(email);
             }
         }
-
         br.close();
-
         System.out.println("Duplicate Emails:");
-
         for (String dup : duplicates) {
             System.out.println(dup);
         }
     }
+
 }
